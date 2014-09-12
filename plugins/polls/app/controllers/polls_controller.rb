a2428d89c271a46d5fc7108dbb68ed12e4d8f156
eduardoschneiders <eduardo.m.schneiders@gmail.com>
2014-09-11 23:59:41 -0300

    Create basic poll plugin

---
+++ plugins/polls/app/controllers/polls_controller.rb
@@ -0,0 +1,18 @@
+class PollsController < ApplicationController
+  unloadable
+
+
+  def index
+		@project = Project.find(params[:project_id])
+		@polls = Poll.all
+  end
+
+  def vote
+		poll = Poll.find(params[:id])
+		poll.vote(params[:answer])
+		if poll.save
+			flash[:notice] = 'Vote saved.'
+		end
+		redirect_to action: :index
+  end
+end
---
+++ plugins/polls/init.rb
@@ -0,0 +1,10 @@
+Redmine::Plugin.register :polls do
+  name 'Polls plugin'
+  author 'Eduardo Schneiders'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+	permission :polls, { polls: [:index, :vote] }, public: true
+	menu :project_menu, :polls, { controller: 'polls', action: 'index' }, caption: 'Polls', after: :activity, param: :project_id
+end
